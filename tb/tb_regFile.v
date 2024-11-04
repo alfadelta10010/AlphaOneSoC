f457bf7ca5551b6a4f0eed81f1e8d7d278961a96
Abhiram Dasika <alfadelta10010@gmail.com>
2024-11-04 18:04:01 +0530

    updated testbench with correct symbos

--- tb/tb_regFile.v
+++ tb/tb_regFile.v
@@ -1,23 +1,21 @@
-//https://edaplayground.com/x/uuSe
-
-module tb_RegisterFile(clk_tb, wen_tb, rd_tb, rs1_tb, rs2_tb, din_tb, r1_tb, r2_tb);
-  output logic clk_tb, wen_tb;
-  output logic [4:0] rd_tb;
-  output logic [4:0] rs1_tb;
-  output logic [4:0] rs2_tb;
-  output logic [31:0] din_tb;
-  input logic [31:0] r1_tb;
-  input logic [31:0] r2_tb;
+module tb_regFile();
+  reg clk_tb, wrEn_tb;
+  reg [4:0] rd_tb;
+  reg [4:0] rs1_tb;
+  reg [4:0] rs2_tb;
+  reg [31:0] dIn_tb;
+  wire [31:0] r1_tb;
+  wire [31:0] r2_tb;
   
-  ///*
-  RegisterFile regs (.clk(clk_tb), 
-             .wen(wen_tb), 
-             .rd(rd_tb), 
-             .rs1(rs1_tb), 
-             .rs2(rs2_tb), 
-             .r1(r1_tb),
-             .r2(r2_tb),
-             .din(din_tb));
+  regFile dut (.clk(clk_tb), 
+               .wen(wrEn_tb), 
+               .rd(rd_tb), 
+               .rs1(rs1_tb), 
+               .rs2(rs2_tb), 
+               .r1(r1_tb),
+               .r2(r2_tb),
+               .dIn(dIn_tb)
+              );
   
   always
     begin
@@ -26,13 +24,13 @@ module tb_RegisterFile(clk_tb, wen_tb, rd_tb, rs1_tb, rs2_tb, din_tb, r1_tb, r2_
       clk_tb = 1;
       #5;
     end
-  //*/
+  
   initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(0, tb_RegisterFile);
+    $dumpfile("regFile.vcd");
+    $dumpvars(0, tb_regFile);
     rd_tb = 5'b01010;
     din_tb = 32'hBABEFACE;
-    #2 wen_tb = 1;
+    #2 wrEn_tb = 1;
     #2 rs1_tb = 5'b00010;
     rs2_tb = 5'b01010;
     #5 rs2_tb = 5'bX;
