e53863a29c9922ef64578eba664004f9c087e4f9
Abhiram Dasika <alfadelta10010@gmail.com>
2024-11-05 21:10:15 +0530

    added cautionary note

--- src/regFile.v
+++ src/regFile.v
@@ -14,9 +14,9 @@ module regFile #(parameter DATA_WIDTH = 32) (clk, wrEn, rs1, rs2, rd, r1, r2, rs
   always @(posedge clk or posedge rst) begin
     if (rst) begin
       reg_file[0] <= 0;
-      reg_file[1] <= 0;
-      reg_file[2] <= 0;
-      reg_file[3] <= 0;
+      reg_file[1] <= 0;  // substitute with other values
+      reg_file[2] <= 0;  // substitute with other values
+      reg_file[3] <= 0;  // substitute with other values
       reg_file[4] <= 0;
       reg_file[5] <= 0;
       reg_file[6] <= 0;
