4055d28633480def0c1e49283037bccd544ced4a
Abhiram Dasika <alfadelta10010@gmail.com>
2024-11-07 15:50:18 +0530

    removed exclamations in testbench

--- tb/tb_signExt.v
+++ tb/tb_signExt.v
@@ -21,13 +21,13 @@ module tb_signExt();
     #1 opcode_tb = 7'b0000011; // Load-type
     instIn_tb = 32'h00c18083;  // 0x0000000C
     #1 opcode_tb = 7'b0010011; // I-type - SRAI
-    instIn_tb = 32'h4070d093;  // 7 !
+    instIn_tb = 32'h4070d093;  // 0x00000007
     #1 opcode_tb = 7'b1100011; // B-type
     instIn_tb = 32'hfe000ae3;  // 0xFFFFFFF4
     #1 opcode_tb = 7'b1100111; // JALR
     instIn_tb = 32'h010100e7;  // 0x00000010
     #1 opcode_tb = 7'b1101111; // JAL
-    instIn_tb = 32'h004000ef;  // 0x00000010 !
+    instIn_tb = 32'h004000ef;  // 0x00000004
     #1 opcode_tb = 7'b0110111; // LUI
     instIn_tb = 32'hfffff037;  // 0xFFFFF000
     #1 opcode_tb = 7'b0010111; // AUIPC
