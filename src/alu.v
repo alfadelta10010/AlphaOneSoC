a096ab279add9a05fa995c7c6c034f9ebcbffa37
Abhiram Dasika <alfadelta10010@gmail.com>
2024-11-10 13:08:13 +0530

    added ALU ready flag for additions

--- src/alu.v
+++ src/alu.v
@@ -1,8 +1,9 @@
-module alu(d1, d2, result, control);
+module alu(d1, d2, result, control, aluReady);
   input wire [31:0] d1;
   input wire [31:0] d2;
   output wire [31:0] result;
   input wire [3:0] control;
+  output wire aluReady;
   
   wire [63:0] sext_rs1 = {{32{d1[31]}}, d1};
   wire [63:0] sra_rslt = sext_rs1 >> d2[4:0];
@@ -11,6 +12,24 @@ module alu(d1, d2, result, control);
   wire LT = (d1[31] ^ d2[31]) ? d1[31] : sum[32];
   wire LTU = sum[32];
   
+  always @(*) 
+    begin
+      aluReady = 1'b0;
+      case (control)
+        4'b0000: begin result = d1 + d2; aluReady = 1'b1; end    // ADD
+        4'b0001: result = d1 << d2[4:0];                      // SLL
+        4'b0010: result = (LT ? 32'h00000001 : 32'h00000000); // SLT
+        4'b0011: result = (LTU ? 32'h00000001 : 32'h00000000);// SLTU
+        4'b0100: result = d1 ^ d2;                            // XOR
+        4'b0101: result = d1 >> d2[4:0];                      // SRL
+        4'b0110: result = d1 | d2;                            // OR
+        4'b0111: result = d1 & d2;                            // AND
+        4'b1000: result = d1 - d2;                            // SUB
+        4'b1101: result = sra_rslt[31:0];                     // SRA
+        default: result = 32'b0;
+      endcase
+    end
+  /*
   assign result = (control == 4'b0000) ? (d1 + d2) :                           // ADD
                   (control == 4'b0001) ? (d1 << d2[4:0]) :                     // SLL
                   (control == 4'b0010) ? (LT ? 32'h00000001 : 32'h00000000) :  // SLT
@@ -22,4 +41,5 @@ module alu(d1, d2, result, control);
                   (control == 4'b1000) ? (d1 - d2) :                           // SUB
                   (control == 4'b1101) ? sra_rslt  :                           // SRA
                   32'b0;
+  */
 endmodule
